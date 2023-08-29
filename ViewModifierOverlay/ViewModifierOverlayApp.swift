3bd5be0e6f7117fcee48767856441d2f01e047c0
Goyo <dur123en@gmail.com>
2023-08-28 19:24:49 -0500

    Initial Commit

---
+++ ViewModifierOverlay/ViewModifierOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifierOverlayApp.swift
+//  ViewModifierOverlay
+//
+//  Created by Enrique on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifierOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
